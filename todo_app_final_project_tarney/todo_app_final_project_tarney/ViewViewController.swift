ebc21495674a1aac5c181c9cdffab322ab43ebe5
1amBulletproof <brandon.tarney@gmail.com>
2018-04-18 18:37:11 -0400

    +more gui framework, +create viewcontrollers

---
+++ todo_app_final_project_tarney/todo_app_final_project_tarney/ViewViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewViewController.swift
+//  todo_app_final_project_tarney
+//
+//  Created by Brandon Tarney on 4/18/18.
+//  Copyright © 2018 Johns Hopkins University. All rights reserved.
+//
+
+import UIKit
+
+class ViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- todo_app_final_project_tarney/todo_app_final_project_tarney/ViewsAndTagsViewController.swift
+++ todo_app_final_project_tarney/todo_app_final_project_tarney/ViewsAndTagsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewUITableViewController: UITableViewController {
+class ViewsAndTagsViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
